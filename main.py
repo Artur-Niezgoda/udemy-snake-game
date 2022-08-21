c36d08f9dc50651f314298917b278d5f818b977f
Artur-Niezgoda <110778550+Artur-Niezgoda@users.noreply.github.com>
2022-08-22 00:52:31 +0200

    add colision rules and snake extend

--- main.py
+++ main.py
@@ -30,16 +30,17 @@ screen.onkey(snake.right, "Right")
 game_is_on = True
 while game_is_on:
     screen.update()
-    time.sleep(0.05)
+    time.sleep(0.1)
     snake.move()
 
     if snake.head.distance(food) < 15:
         food.refresh()
         scoreboard.score += 1
         scoreboard.display_score()
+        snake.extend()
     
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() <-280 or snake.head.ycor() > 280:
         game_is_on = False
-    
+        scoreboard.game_over()
 
 screen.exitonclick()
--- snake.py
+++ snake.py
@@ -1,6 +1,6 @@
 from turtle import Turtle
 
-INIT_NO = 3
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
 MOVE_DISTANCE = 20
 UP = 90
 DOWN = 270
@@ -17,13 +17,21 @@ class Snake:
         self.create_snake()
         self.head = self.segments[0]
 
+
     def create_snake(self):
-        for i in range(INIT_NO):
-            new_segment = Turtle("square")
-            new_segment.color("white")
-            new_segment.penup()
-            new_segment.setx(-i*20)
-            self.segments.append(new_segment)
+        for position in STARTING_POSITIONS:
+            self.add_segment(position)
+            
+    
+    def add_segment(self, position):
+        new_segment = Turtle("square")
+        new_segment.color("white")
+        new_segment.penup()
+        new_segment.goto(position)
+        self.segments.append(new_segment)
+
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
 
     def move(self):
         for seg_num in range(len(self.segments)-1, 0, -1):
